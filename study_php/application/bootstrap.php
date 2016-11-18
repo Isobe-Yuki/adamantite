13a7249acdc6b546a20fa8575ca9bed3ef4f71b6
YUUKI-PC\yuuki <isobe.yuuki.z9h3008@gmail.com>
2016-11-18 23:35:40 +0900

    test

---
+++ study_php/application/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'core/ClassLoader.php';
+
+$loader = new ClassLoader();
+$loader->registerDirectory(dirname(__FILE__) . '/core');
+$loader->registerDirectory(dirname(__FILE__) . '/models');
+$loader->register();
+
+?>
---
+++ study_php/application/core/ClassLoader.php
@@ -0,0 +1,24 @@
+<?php
+
+class ClassLoader {
+	protected $directories;
+	
+	public function register () {
+		spl_autoload_register(array($this, 'loadClass'));
+	}
+	
+	public function registerDirectory ($directory) {
+		$this->directories[] = $directory;
+	}
+	
+	public function loadClass ($class) {
+		foreach ($this->directories as $directory) {
+			$file = $directory . '/' . $class . '.php';
+			if (is_readable($file)) {
+				require $file;
+				return;
+			}
+		}
+	}
+}
+?>
